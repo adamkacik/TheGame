a9e5cd5c50a2b2fa846c04eb37977760787a7d3e
Adam <adamkacik1988@gmail.com>
2018-12-14 11:53:01 +0100

    Loading level files

--- src/com/adamkacik/game/level/Level.java
+++ src/com/adamkacik/game/level/Level.java
@@ -27,7 +27,7 @@ public class Level {
 	}
 		}
 	}
-	private void loadLevel(String path) {
+	protected void loadLevel(String path) {
 		
 	}
 	
---
+++ src/com/adamkacik/game/level/SpawnLevel.java
@@ -0,0 +1,38 @@
+package com.adamkacik.game.level;
+
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+import com.adamkacik.game.level.tile.Tile;
+
+public class SpawnLevel extends Level{
+
+	private Tile[] tiles;
+	private int[] levelPixels;
+	
+	public SpawnLevel(String path) {
+		super(path);
+	}
+	
+	protected void loadLevel(String path) {
+		try {
+			BufferedImage image = ImageIO.read(SpawnLevel.class.getResource(path));
+			int w =image.getWidth();
+			int h = image.getHeight();
+			image.getRGB(0, 0,w,h,levelPixels,0,w);
+		} catch (IOException e) {
+			System.out.println("Exception!! Could not load level file!!");
+		}
+	}
+	protected void generateLevel() {
+		
+	}
+	
+	public SpawnLevel(int width, int height) {
+		super(width, height);
+
+	}
+
+}
