53c524f2b7f6b3bb2dd9ff682dd1f6396d5a9758
Adam <adamkacik1988@gmail.com>
2019-02-04 20:22:14 +0100

    Initial Commit

--- src/com/adamkacik/game/entity/particle/Particle.java
+++ src/com/adamkacik/game/entity/particle/Particle.java
@@ -1,5 +1,41 @@
 package com.adamkacik.game.entity.particle;
 
-public class Particle {
+import java.util.ArrayList;
+import java.util.List;
 
+import com.adamkacik.game.entity.Entity;
+import com.adamkacik.game.graphics.Screen;
+import com.adamkacik.game.graphics.Sprite;
+
+public class Particle extends Entity{
+	
+	private List<Particle> particles = new ArrayList<Particle>();
+	private Sprite sprite;
+	
+	private int life;
+	
+	public Particle(int x, int y, int life) {
+		this.x = x;
+		this.y = y;
+		this.life = life;
+		sprite = Sprite.particle_normal;
+		particles.add(this);
+		
+	}
+	
+	public Particle(int x, int y, int life, int amount) {
+		this(x, y, life);
+		for(int i=0;i<amount-1;i++) {
+			particles.add(new Particle(x,y,life));
+			
+		}
+	}
+	public void update() {
+		
+	}
+	public void render(Screen screen) {
+		
+	}
+	
+	
 }
