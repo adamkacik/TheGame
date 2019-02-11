c11772fe09ac2559a47dd0b67abc0b4772dce662
Adam <adamkacik1988@gmail.com>
2019-02-11 20:37:07 +0100

    Particle Simulation

--- src/com/adamkacik/game/entity/particle/Particle.java
+++ src/com/adamkacik/game/entity/particle/Particle.java
@@ -18,7 +18,7 @@ public class Particle extends Entity {
 	protected double xx, yy, zz;
 	
 	public Particle(int x, int y, int life) {
-		System.out.println("Particle life: " + life);
+		//System.out.println("Particle life: " + life);
 		this.x = x;
 		this.y = y;
 		this.xx = x;
@@ -26,8 +26,7 @@ public class Particle extends Entity {
 		this.life = life +(random.nextInt(50)-25);
 		sprite = Sprite.particle_normal;
 		
-		this.xa = random.nextGaussian()+0.65;
-		if(this.xa<0)xa=0.1;
+		this.xa = random.nextGaussian();
 		this.ya = random.nextGaussian();
 		this.zz=random.nextFloat()+2.0;
 
@@ -51,16 +50,41 @@ public class Particle extends Entity {
 		if(zz < 0) {		//add some "gravity"
 			zz=0;
 			za *= -0.65;
-			xa *=0.4;
+			xa *= 0.4;
 			ya *= 0.4;
+		}
+		move((xx+xa), (yy+ya)+(zz+za));
+	}
+
+	private void move(double d, double e) {
+		if (collision((xx+xa), (yy+ya)+(zz+za))){
+			this.xa *= -0.5;
+			this.ya *= -0.5;
+			this.za *= -0.5;
 		}
 		this.xx += xa;
 		this.yy += ya;
 		this.zz += za;
+		
 	}
 
+	public boolean collision(double x, double y) {	//collision for shooting
+		boolean solid=false;
+		for (int c=0; c<4 ; c++) {
+			double xt=(x - c%2*16)/16;		//size/10+8)/16;
+			double yt=(y - c/2*16)/16;		//size/5+8)/16;	
+			int ix = (int) Math.ceil(xt);
+			int iy = (int) Math.ceil(yt);
+			if (c%2 == 0) ix = (int) Math.floor(xt);
+			if (c/2 == 0) iy = (int) Math.floor(yt);
+			
+		if (level.getTile(ix,iy).solid()) solid =true;
+		}
+		return solid;
+	}
+	
 	public void render(Screen screen) {
-		screen.renderSprite((int) xx, (int) yy - (int)zz, sprite, true);
+		screen.renderSprite((int) xx, (int) yy - (int)zz -1, sprite, true);
 	}
 
 }
