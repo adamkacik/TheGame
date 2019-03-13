64544aaccfb644812d57b359d0fa7d046795ba0a
Adam <adamkacik1988@gmail.com>
2019-03-13 21:59:48 +0100

    Font sheets

---
+++ src/com/adamkacik/game/graphics/Font.java
@@ -0,0 +1,12 @@
+package com.adamkacik.game.graphics;
+
+public class Font {
+	
+	private static SpriteSheet font = new SpriteSheet("/fonts/arial.png",16 );
+	//TODO: Extract sheet cells into individual sprites!
+	//private static SpriteSheet font_characters = new SpriteSheet(font,  );
+	
+	public Font() {
+		
+	}
+}
